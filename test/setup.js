239e52073ff57094e18cce2b159a4ad663d6af2d
John Wellbrock <wellbrock23john@gmail.com>
2020-11-18 18:03:33 -0700

    deployment

--- test/setup.js
+++ test/setup.js
@@ -5,8 +5,9 @@ process.env.JWT_EXPIRY = "3m";
 
 require("dotenv").config();
 
-process.env.TEST_DB_URL =
-	process.env.TEST_DB_URL || "postgresql://postgres@localhost/gentle-test";
+process.env.TEST_DATABASE_URL =
+	process.env.TEST_DATABASE_URL ||
+	"postgresql://postgres@localhost/gentle-test";
 
 const { expect } = require("chai");
 const supertest = require("supertest");
